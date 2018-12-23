fa1ca3a03f27f34a9e72a96f940bd3df927abde2
Bus42 <gbrewton1980@gmail.com>
2018-12-23 15:17:54 -0700

    Completes #12

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import Notifications from "./Notifications";
 import ProjectList from '../projects/ProjectList';
+import { connect } from 'react-redux';
+
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
   render() {
+    const { projects } = this.props;
+    //console.log(this.props);
     return (
       <div className="dashboard container">
         <div className="row">
-          <div className="col s12 m6"><ProjectList/></div>
+          <div className="col s12 m6"><ProjectList projects={projects} /></div>
           <div className="col s12 m5 offset-m1"></div>
           <Notifications />
         </div>
@@ -19,4 +23,10 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,22 @@
-const initState = {};
+const initState = {
+    projects: [
+        {
+            id: '1', 
+            title: 'Help me find peach', 
+            content: 'Culpa incididunt ea nulla id elit adipisicing.'
+        },
+        {
+            id: '2', 
+            title: 'Collect all the start', 
+            content: 'Nulla ad cupidatat incididunt sint consectetur voluptate aute et amet laboris aliqua anim Lorem.'
+        },
+        {
+            id: '3', 
+            title: 'egg hunt with yoshi', 
+            content: 'Adipisicing velit occaecat esse dolore cillum eiusmod minim.'
+        }
+    ]
+};
 
 const projectReducer = (state = initState, action) => {
     return state;
