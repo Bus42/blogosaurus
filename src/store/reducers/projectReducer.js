166d4f199b6f8385f0bb4de45ff40c21b94bddba
Bus42 <bus42@studio42dev.com>
2018-12-26 13:29:53 -0700

    Completes #18 Adding Data to Firestore

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,25 +1,35 @@
 const initState = {
-    projects: [
-        {
-            id: '1', 
-            title: 'Help me find peach', 
-            content: 'Culpa incididunt ea nulla id elit adipisicing.'
-        },
-        {
-            id: '2', 
-            title: 'Collect all the stars', 
-            content: 'Nulla ad cupidatat incididunt sint consectetur voluptate aute et amet laboris aliqua anim Lorem.'
-        },
-        {
-            id: '3', 
-            title: 'egg hunt with yoshi', 
-            content: 'Adipisicing velit occaecat esse dolore cillum eiusmod minim.'
-        }
-    ]
+  projects: [
+    {
+      id: "1",
+      title: "Help me find peach",
+      content: "Culpa incididunt ea nulla id elit adipisicing."
+    },
+    {
+      id: "2",
+      title: "Collect all the stars",
+      content:
+        "Nulla ad cupidatat incididunt sint consectetur voluptate aute et amet laboris aliqua anim Lorem."
+    },
+    {
+      id: "3",
+      title: "egg hunt with yoshi",
+      content: "Adipisicing velit occaecat esse dolore cillum eiusmod minim."
+    }
+  ]
 };
 
 const projectReducer = (state = initState, action) => {
-    return state;
-}
- 
+  switch (action.type) {
+    case "CREATE_PROJECT":
+      console.log("created project", action.project);
+      return state;
+    case "CREATE_PROJECT_ERROR":
+      console.log("create project error", action.err);
+      return state;
+    default:
+      return state;
+  }
+};
+
 export default projectReducer;
