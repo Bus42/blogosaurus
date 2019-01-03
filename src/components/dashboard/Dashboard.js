eb18ce1e40511ca049faab64eba4196d870cc45a
Bus42 <bus42@studio42dev.com>
2019-01-03 11:11:40 -0700

    Completes #35

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,32 +1,35 @@
-import React, { Component } from "react";
-import Notifications from "./Notifications";
-import ProjectList from '../projects/ProjectList';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
-import { Redirect } from "react-router-dom";
+import React, { Component } from 'react'
+import ProjectList from '../projects/ProjectList'
+import Notifications from './Notifications'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
+import { Redirect } from 'react-router-dom'
+
+
 
 class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
   render() {
     const { projects, auth } = this.props;
-    if(!auth.uid)return <Redirect to='/signin' />
+    if (!auth.uid) return <Redirect to='/signin' /> 
+
     return (
       <div className="dashboard container">
         <div className="row">
-          <div className="col s12 m6"><ProjectList projects={projects} /></div>
-          <div className="col s12 m5 offset-m1"></div>
-          <Notifications />
+          <div className="col s12 m6">
+            <ProjectList projects={projects} />
+          </div>
+          <div className="col s12 m5 offset-m1">
+            <Notifications />
+          </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = (state) => {
+  // console.log(state);
   return {
     projects: state.firestore.ordered.projects,
     auth: state.firebase.auth
@@ -38,4 +41,4 @@ export default compose(
   firestoreConnect([
     { collection: 'projects' }
   ])
-)(Dashboard);
+)(Dashboard)
