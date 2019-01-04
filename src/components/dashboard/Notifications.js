d1915257777a2a78e75c461e22d4735f533e168b
Bus42 <bus42@studio42dev.com>
2019-01-04 13:23:42 -0700

    Completes tutorial series, deploys to firebase

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,21 +1,26 @@
-import React from 'react';
+import React from 'react'
+import moment from 'moment'
 
 const Notifications = (props) => {
-    return ( <div className="section">
-        <div className="card z-depth-0">
-            <div className="card-content">
-                <span className="card-title">Notifications</span>
-                <ul className="notifications">
-                    <li>Notification</li>
-                    <li>Notification</li>
-                    <li>Notification</li>
-                    <li>Notification</li>
-                    <li>Notification</li>
-                    <li>Notification</li>
-                </ul>
-            </div>
+  const { notifications } = props;
+  return (
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="online-users">
+            { notifications && notifications.map(notification =>{
+              return <li key={notification.id}>
+                <span className="pink-text">{notification.user} </span>
+                <span>{notification.content}</span>
+                <div className="note-date grey-text">{moment(notification.time.toDate()).fromNow()}</div>
+              </li>
+            })}
+          </ul>
         </div>
-    </div> );
+      </div>
+    </div>
+  )
 }
- 
-export default Notifications;
+
+export default Notifications
