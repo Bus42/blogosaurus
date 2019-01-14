f7bb1e457091890ccaf6ba8e89fdf98b532ea71d
Bus42 <gbrewton1980@gmail.com>
2019-01-14 14:56:15 -0700

    Adjusts layout to optimize for mobile display

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { signIn } from "../../store/actions/authActions";
 import { Redirect } from "react-router-dom";
-import Welcome from '../layout/Welcome';
+import Welcome from "../layout/Welcome";
 
 class SignIn extends Component {
   state = {
@@ -21,42 +21,52 @@ class SignIn extends Component {
 
   render() {
     const { authError, auth } = this.props;
-    if(auth.uid)return <Redirect to='/' />
+    if (auth.uid) return <Redirect to="/" />;
     return (
       <div className="container">
-        <Welcome/>
-        <form onSubmit={this.handleSubmit} className="white">
-          <h5 className="grey-text text-darken-3">Sign In</h5>
-          <div className="input-field">
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" onChange={this.handleChange} />
+        <div className="row">
+          <Welcome />
+          <div className="col s12 m6">
+            <form onSubmit={this.handleSubmit} className="white">
+              <h5 className="grey-text text-darken-3">Sign In</h5>
+              <div className="input-field">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" onChange={this.handleChange} />
+              </div>
+              <div className="input-field">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  onChange={this.handleChange}
+                />
+              </div>
+              <div className="input-field">
+                <button className="btn light-green lighten-1 z-depth-0">
+                  Login
+                </button>
+                <div className="red-text center">
+                  {authError ? <p>{authError}</p> : null}
+                </div>
+              </div>
+            </form>
           </div>
-          <div className="input-field">
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <button className="btn light-green lighten-1 z-depth-0">Login</button>
-            <div className="red-text center">
-              { authError ? <p>{authError}</p> : null }
-            </div>
-          </div>
-        </form>
+        </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     authError: state.auth.authError,
     auth: state.firebase.auth
-  }
-}
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
-    signIn: (creds) => dispatch(signIn(creds))
+    signIn: creds => dispatch(signIn(creds))
   };
 };
 
--- src/components/layout/Welcome.js
+++ src/components/layout/Welcome.js
@@ -3,47 +3,47 @@ import { NavLink } from "react-router-dom";
 
 const Welcome = () => {
   return (
-    <div className="row">
-      <div className="col s12 m8">
-        <div className="card blue-grey darken-1">
-          <div className="card-content white-text">
-            <span className="card-title">Team Communication</span>
-            <p>
-              Use this for secure, instant communication requiring
-              authentication that will enable record-keeping of communications
-              as well. This can be used from home, in the office, or wherever
-              you are.
-            </p>
-          </div>
+    <div className="col s12 m6">
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">Team Communication</span>
+          <p>
+            Use this for secure, instant communication requiring authentication
+            that will enable record-keeping of communications as well. This can
+            be used from home, in the office, or wherever you are.
+          </p>
         </div>
-        <div className="card blue-grey darken-1">
-          <div className="card-content white-text">
-            <span className="card-title">Project Planning</span>
-            <p>
-              List topics and get feedback from collaborators on each topic to
-              guide you while planning your next party, fundraiser, or company
-              outing.
-            </p>
-          </div>
+      </div>
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">Project Planning</span>
+          <p>
+            List topics and get feedback from collaborators on each topic to
+            guide you while planning your next party, fundraiser, or company
+            outing.
+          </p>
         </div>
-        <div className="card blue-grey darken-1">
-          <div className="card-content white-text">
-            <span className="card-title">Blogging!</span>
-            <p>
-              This app can easily be tailored to suit the purposes of any blog,
-              even a community blog. I can make it searchable, add categories
-              and custom filters, add the ability to upload images or embed
-              videos - I can make it exactly what you need it to be for your
-              blog, wedding page, band site, or whatever you need it to be.
-            </p>
-          </div>
+      </div>
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">Blogging!</span>
+          <p>
+            This app can easily be tailored to suit the purposes of any blog,
+            even a community blog. I can make it searchable, add categories and
+            custom filters, add the ability to upload images or embed videos - I
+            can make it exactly what you need it to be for your blog, wedding
+            page, band site, or whatever you need it to be.
+          </p>
         </div>
-        <div className="card blue-grey darken-1">
-          <div className="card-action">
-            <p className="white-text">
-              <NavLink className="waves-effect waves-light btn" to="/signUp">Sign up</NavLink> to get started or sign in below if you already have an account.
-            </p>
-          </div>
+      </div>
+      <div className="card blue-grey darken-1">
+        <div className="card-action">
+          <p className="white-text">
+            <NavLink className="waves-effect waves-light btn" to="/signUp">
+              Sign up
+            </NavLink>{" "}
+            to get started or sign in if you already have an account.
+          </p>
         </div>
       </div>
     </div>
