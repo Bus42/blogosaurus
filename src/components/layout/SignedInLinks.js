4be1424507ed15dc332899a679c14d907a5743d3
Bus42 <gbrewton1980@gmail.com>
2019-01-14 16:55:59 -0700

    Adds icons to New Blog Post and Sign Out buttons

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,20 +1,61 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { signOut } from '../../store/actions/authActions'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
 
-const SignedInLinks = (props) => {
-    return ( <ul className="right">
-        <li><NavLink to='/create'>New Blog Entry</NavLink></li>
-        <li><a href="/" onClick={props.signOut}>Sign Out</a></li>
-        <li><NavLink to='/' className='btn btn-floating light-green lighten-1' >{props.profile.initials}</NavLink></li>
-    </ul> );
-}
+const SignedInLinks = props => {
+  return (
+    <ul className="right">
+      <li>
+        <NavLink to="/create">
+          <i style={{ display: "inline" }} className="material-icons">
+            add_to_photos
+          </i>
+          <span
+            className="navSpan"
+            style={
+              window.innerWidth < 901
+                ? { display: "none" }
+                : { display: "inline" }
+            }
+          >
+            New Blog Entry
+          </span>
+        </NavLink>
+      </li>
+      <li>
+        <a href="/" onClick={props.signOut}>
+          <i style={{ display: "inline" }} className="material-icons">
+            eject
+          </i>
+          <span
+            className="navSpan"
+            style={
+              window.innerWidth < 901
+                ? { display: "none" }
+                : { display: "inline" }
+            }
+          >
+            Sign Out
+          </span>
+        </a>
+      </li>
+      <li>
+        <NavLink to="/" className="btn btn-floating light-green lighten-1">
+          {props.profile.initials}
+        </NavLink>
+      </li>
+    </ul>
+  );
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        signOut: () => dispatch(signOut())
-    }
-}
- 
-export default connect(null, mapDispatchToProps)(SignedInLinks);
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SignedInLinks);
